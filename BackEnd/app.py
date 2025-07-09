5878549c0e533a3e2489e6c205ac96abaa64aea9
B Yaswini Naidu <yaswiniyashu3@gmail.com>
2025-07-09 09:55:30 +0530

    backend commit 1

---
+++ BackEnd/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+
+app = Flask(__name__)
+api = Api(app)
+
+class Login(Resource):
+    def post(self):
+        return'Hello World!'
+
+
+api.add_resource(Login, '/login')   
+
+if __name__ == '__main__':
+    app.run()
+
